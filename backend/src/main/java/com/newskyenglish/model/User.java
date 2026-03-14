040d5f5721f96b0d3d88a3acc48a7ca9930a491e
blong04 <long0961511354@gmail.com>
2026-03-14 14:14:03 +0700

    fix user.java

--- backend/src/main/java/com/newskyenglish/model/User.java
+++ backend/src/main/java/com/newskyenglish/model/User.java
@@ -18,7 +18,7 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "UserID")
+    @Column(name = "userid")
     private Long id;
 
     @NotBlank(message = "Họ tên không được để trống")
