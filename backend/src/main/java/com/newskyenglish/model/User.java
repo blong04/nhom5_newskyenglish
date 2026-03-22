2770f15a144d269705662453561c4465faedc17a
blong04 <long0961511354@gmail.com>
2026-03-22 15:01:36 +0700

    chỉnh sửa cấu trúc thư mục

--- backend/src/main/java/com/newskyenglish/model/User.java
+++ backend/src/main/java/com/newskyenglish/model/User.java
@@ -3,8 +3,6 @@ package com.newskyenglish.model;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.*;
 import lombok.*;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
 
